45dfcd295f44fc245d8fc8dd8406705486e3bc3f
Matt Payne <Payne@MattPayne.org>
2019-07-04 10:51:33 -0500

    Now a JPA entity

--- src/main/java/dev/dustinwright/kbspring2/Kb.java
+++ src/main/java/dev/dustinwright/kbspring2/Kb.java
@@ -2,8 +2,16 @@ package dev.dustinwright.kbspring2;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Kb {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long kbNumber;
 	private String title;
 	private String keywords;
